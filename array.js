b36b9fa0c16c16a12cc1925fbd1cd6419ac9cb1b
Andrew Shaffer <darawk@gmail.com>
2015-03-07 01:33:25 -0800

    overhaul array to be compatible with prototypical methods

--- array.js
+++ array.js
@@ -2,43 +2,57 @@ var isEmpty = require('lodash.isempty');
 var extend = require('lodash.assign');
 var isArray = require('lodash.isarray');
 var toArray = require('lodash.toarray');
+var _ = require('lodash');
 
 module.exports = function(model) {
   var base = model
-    .validator(isArray, 'array')
+    .validator(function(val) {
+      return val instanceof ArrayDocument;
+    }, 'array')
     .caster(toArray)
     .default([])
     .requiredValidator(function(value) {
       return value && value.length > 0;
-    })
-    .on('init', function(evt) {
-      var rawDoc = evt.doc.doc === undefined || isEmpty(evt.doc.doc)
-          ? []
-          : evt.doc.doc;
-      
-      // Arrays are kind of intense, so who knows what might happen
-      // if we let a non-array in here
-      if(! isArray(rawDoc))
-        throw new Error('Cannot construct non-array as array type');
-      
-      var doc = extend(rawDoc, arrayMixin, evt.doc);
-      // This is self-referential, so that .get/.set
-      // are properly
-      doc.doc = doc;
-      
-      doc.forEach(function(item, idx) {
-        doc.get(idx);
-      });
-
-      evt.doc = doc;
-    })
-    .type('array', function(type) {
-      return base.attr('*', type);
     });
+  
+  base = createArray(base);
+
+  var use = base.use;
+  // Shim .use so we can pass down our array-ness to
+  // our children
+  base.use = function() {
+    return createArray(use.apply(this, arguments));
+  };
+  
+  base.on('init', function(evt) {
+    refresh(evt.doc);
+  });
+
+  base.type('array', function(type) {
+    return base.attr('*', type);
+  });
 };
 
+function createArray(model) {
+  var ArrayModel = function(arr) {
+    arr = arr || [];
+    var self = model.call(this, this);
+    ArrayDocument.call(self, arr);
+    return self;
+  };
+  ArrayModel.prototype = Object.create(ArrayDocument.prototype);
+  extend(ArrayModel.prototype, model.prototype);
+  extend(ArrayModel, model);
+  return ArrayModel;  
+}
 
-var arrayMixin = {};
+
+function ArrayDocument(arr) {
+  this.push.apply(this, arr);
+}
+
+ArrayDocument.prototype = Object.create(Array.prototype);
+
 // We only care about the subset of array mutators
 // that may add new items.  All we care about
 // is ensuring that array elements have been coerced
@@ -59,9 +73,9 @@ function refresh(arr) {
 }
 
 mutators.forEach(function(method) {
-  arrayMixin[method] = function() {
+  ArrayDocument.prototype[method] = function() {
     var res = Array.prototype[method].apply(this, arguments);
     refresh(this);
     return res;
   }
-})
+});
